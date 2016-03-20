4270ffca4a743705dd35e241898e5e58a8643e8c
Matthew Barnes <mbarnes@redhat.com>
2016-03-19 22:29:54 -0400

    [mbarnes] Avoid X11
    Not running an X server on the arcade machine.
    
    This required some hacking of bgfx for the assumption that
    BX_PLATFORM_LINUX implies the use of X11 libraries.

--- 3rdparty/bgfx/src/glcontext_glx.cpp
+++ 3rdparty/bgfx/src/glcontext_glx.cpp
@@ -97,6 +97,7 @@ namespace bgfx { namespace gl
 		m_context = (GLXContext)g_platformData.context;
 		m_display = (::Display*)g_platformData.ndt;
 
+#if 0
 		// It's possible the user has provided the window handle, but not
 		// the display handle. If this is the case, try opening the default
 		// display
@@ -223,6 +224,7 @@ namespace bgfx { namespace gl
 
 			XUnlockDisplay(m_display);
 		}
+#endif
 
 		import();
 
@@ -284,6 +286,7 @@ namespace bgfx { namespace gl
 	void GlContext::destroy()
 	{
 		glXMakeCurrent(m_display, 0, 0);
+#if 0
 		if (NULL == g_platformData.context)
 		{
 			glXDestroyContext(m_display, m_context);
@@ -294,6 +297,7 @@ namespace bgfx { namespace gl
 		{
 			XCloseDisplay(m_display);
 		}
+#endif
 		m_context    = NULL;
 		m_visualInfo = NULL;
 		m_display    = NULL;
